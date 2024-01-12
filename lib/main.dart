5a0bd0e2661ecb46470a97c3032613edbe244d7e
SanqBear <sanqbear@outlook.com>
2024-01-12 18:35:13 +0900

    refactor: add bootstrap logic for catching exception

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,14 @@
 import 'package:flutter/material.dart';
+
+import 'package:sanq_viewer/src/bootstrap.dart';
+import 'package:sanq_viewer/src/setting/setting_controller.dart';
 import 'package:sanq_viewer/src/setting/setting_service.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final settingController = SettingController(SettingService());
   await settingController.load();
-  runApp(const MainApp());
-}
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  bootstrap(settingController: settingController);
 }
