73beadeb24c8d8059e9d6d2a8e34bc7e2757838e
SanqBear <sanqbear@outlook.com>
2024-01-17 15:05:08 +0900

    chore: add util.dart

---
+++ lib/src/util/util.dart
@@ -0,0 +1,5 @@
+bool isUrl(String url) {
+  return RegExp(
+          r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
+      .hasMatch(url);
+}
