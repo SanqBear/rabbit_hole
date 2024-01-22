1aca79bac2b117a3bf66a3084d266df73c03bdd0
SanqBear <sanqbear@outlook.com>
2024-01-22 17:08:14 +0900

    chore: remove util component with validators package

--- lib/src/util/util.dart
+++
@@ -1,5 +0,0 @@
-bool isUrl(String url) {
-  return RegExp(
-          r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
-      .hasMatch(url);
-}
